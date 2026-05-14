06036c7316f436f0a6644065831995f611af2bff
AndersonPinho <39173102+AndersonPinho@users.noreply.github.com>
2026-05-14 07:40:25 -0300

    Criando função tower e removendo o código da Main.

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ var roleHarvester = require('role.harvester');
 var roleUpgrader = require('role.upgrader');
 var roleBuilder = require('role.builder');
 var roleSpawn = require('role.spawn');
+var roleTower = require('role.tower');
 var basename = 'Spawn1'
 
 module.exports.loop = function () {
@@ -15,20 +16,7 @@ module.exports.loop = function () {
 
     roleSpawn.run(basename);
 
-    var tower = Game.getObjectById('f49783a12852af396ab6d992');
-    if(tower) {
-        var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
-            filter: (structure) => structure.hits < structure.hitsMax
-        });
-        if(closestDamagedStructure) {
-            tower.repair(closestDamagedStructure);
-        }
 
-        var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
-        if(closestHostile) {
-            tower.attack(closestHostile);
-        }
-    }
 
     for(var name in Game.creeps) {
         var creep = Game.creeps[name];
