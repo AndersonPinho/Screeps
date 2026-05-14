7c1a3c17f8dd4b30a6dcd8e6004b3d1a513d639e
AndersonPinho <39173102+AndersonPinho@users.noreply.github.com>
2026-05-14 15:59:49 -0300

    Refactor roleTower to remove unnecessary variable

--- role.tower.js
+++ role.tower.js
@@ -2,7 +2,6 @@ var roleTower = {
 
     /** @param {tower} creep **/
     run: function(tower) {
-    var tower = Game.getObjectById('f49783a12852af396ab6d992');
     if(tower) {
         var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
             filter: (structure) => structure.hits < structure.hitsMax
